28d4474b12bef9d9788f8ffb7c216a5cd0277a2c
gelther <lpw.sormano@gmail.com>
2016-03-20 22:55:45 +0100

    Code consistency changes for includes/process-download.php

--- includes/process-download.php
+++ includes/process-download.php
@@ -313,7 +313,7 @@ function edd_deliver_download( $file = '', $redirect = false ) {
  * @return   string
  */
 function edd_get_file_ctype( $extension ) {
-	switch ( $extension ):
+	switch ( $extension ) :
 		case 'ac'       : $ctype = 'application/pkix-attr-cert'; break;
 		case 'adp'      : $ctype = 'audio/adpcm'; break;
 		case 'ai'       : $ctype = 'application/postscript'; break;
