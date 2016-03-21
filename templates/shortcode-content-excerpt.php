8e6919e58cd4a963d9812895457ff0db27831b9f
gelther <lpw.sormano@gmail.com>
2016-03-21 14:48:37 +0100

    Code consistency changes for templates/shortcode-content-excerpt.php

--- templates/shortcode-content-excerpt.php
+++ templates/shortcode-content-excerpt.php
@@ -8,4 +8,5 @@
 	<div itemprop="description" class="edd_download_excerpt">
 		<?php echo apply_filters( 'edd_downloads_excerpt', wp_trim_words( get_post_field( 'post_content', get_the_ID() ), $excerpt_length ) ); ?>
 	</div>
-<?php endif; ?>
+<?php endif;
+?>
